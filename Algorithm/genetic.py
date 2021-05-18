622d0a7a343cb5751126e8cdd2674cadbe0b81a4
Jarno Biesheuvel <0968890@hr.nl>
2021-05-18 14:36:09 +0200

    first genetic commit

---
+++ Algorithm/genetic.py
@@ -0,0 +1,22 @@
+#design
+#   sort of running function
+#   mutation function
+#   create children
+#   remember best child
+#   fitness function
+#   
+#
+
+class genetic:
+    def mutation (self, child):
+        return None
+
+    def run (self):
+        return None
+
+    def createChild (self, number):
+        return number
+    
+    def fitness(self, fitness):
+        return None
+    
--- Source/melodymaker.py
+++ Source/melodymaker.py
@@ -4,7 +4,7 @@ AUTHORS: Alan Zhong, Jarno Biesheuvel, Jozef Chen
 """
 
 def main():
-    print("Hello World")
+    print("Hello World ")
 
 if __name__ == '__main__':
     main()
