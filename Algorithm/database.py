7d624ae43e8e53a7201bcd67f78ba15cc7e337c3
Jarno Biesheuvel <0968890@hr.nl>
2021-05-26 11:09:26 +0200

    moved bach to seperate file

--- Algorithm/database.py
+++ Algorithm/database.py
@@ -4,29 +4,7 @@ import shutil
 import tomita.legacy.pysynth as ps
 # import tomita.legacy.pysynth as ps
 # path in methods should be treated that, folder of python file is home folder 
-bach = (
-    (
-        ('e', 8), ('f#', 8),
-        ('g*', 4), ('f#', 8), ('e', 8), ('d#*', 4), ('e', 8), ('f#', 8),
-        ('b3*', 4), ('c#', 8), ('d#', 8), ('e*', 4), ('d', 8), ('c', 8),
-        ('b3*', 4), ('a3', 8), ('g3', 8), ('f#3*', 4), ('g3', 8), ('a3', 8),
-        ('b3*', 8), ('a3', 8), ('g3', 8), ('f#3', 8), ('e3*', 4), ('e', 8), ('f#', 8),
-        ('g*', 4), ('f#', 8), ('e', 8), ('d#*', 4), ('e', 8), ('f#', 8),
-        ('b3*', 4), ('c#', 8), ('d#', 8), ('e*', 4), ('d', 8), ('c', 8),
-        ('b3*', 4), ('a3', 8), ('g3', 8), ('g3*', 32), ('f#3*', 32), ('g3*', 32), ('f#3*', 32), ('g3*', 32), ('f#3*', 32), ('g3*', 32), ('f#3*', 6.4), ('g3', 8), ('g3*', -2),
-    ),
-    (
-        ('g2', 8), ('f#2', 8),
-        ('e2*', 4), ('a2', 4), ('b2', 4), ('a2', 4),
-        ('g2*', 4), ('f#2', 4), ('e2', 4), ('f#2', 4),
-        ('g2*', 4), ('a2', 4), ('b2', 4), ('a2', 4),
-        ('g2*', 4), ('b2', 4), ('e2', 8), ('f#2', 8), ('g2', 8), ('f#2', 8),
-        ('e2*', 4), ('a2', 4), ('b2', 4), ('a2', 4),
-        ('g2*', 4), ('f#2', 4), ('e2', 4), ('f#2', 4),
-        ('g2*', 4), ('c3', 4), ('d3', 4), ('d3', 4),
-        ('b2*', -2),
-    )
-)
+
 class database:
     
     parent_dir =  os.getcwd()
@@ -61,7 +39,9 @@ class database:
             f.write(line + '\n')
             
     def readFile (name):
+        print(database.parent_dir +name)
         text = open(database.parent_dir +name)
+        
         file = text.readlines()
         print(file)
         text.close()
@@ -92,5 +72,5 @@ class database:
 
 # database.createDirectory("one")
 # database.createFile("","\one\iets.wav",bach)        
-# database.readFile( "\one\iets.wav")
+database.readFile( "\BuildingBlocks\Bach.py")
      
