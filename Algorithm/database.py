0d6f71713d7a8105cd39f9674c5f470d2196037f
Jarno Biesheuvel <0968890@hr.nl>
2021-05-26 16:28:17 +0200

    fix create file issues.

--- Algorithm/database.py
+++ Algorithm/database.py
@@ -6,18 +6,33 @@ import tomita.legacy.pysynth as ps
 # path in methods should be treated that, folder of python file is home folder 
 
 class database:
-
-    m_bpm = 180,
-    m_transpose = 1,
-    m_pause = 0.0,
-    m_boost = 1.3,
-    m_repeat = 2,
-
+    bach = (
+        (
+            ('e', 8), ('f#', 8),
+            ('g*', 4), ('f#', 8), ('e', 8), ('d#*', 4), ('e', 8), ('f#', 8),
+            ('b3*', 4), ('c#', 8), ('d#', 8), ('e*', 4), ('d', 8), ('c', 8),
+            ('b3*', 4), ('a3', 8), ('g3', 8), ('f#3*', 4), ('g3', 8), ('a3', 8),
+            ('b3*', 8), ('a3', 8), ('g3', 8), ('f#3', 8), ('e3*', 4), ('e', 8), ('f#', 8),
+            ('g*', 4), ('f#', 8), ('e', 8), ('d#*', 4), ('e', 8), ('f#', 8),
+            ('b3*', 4), ('c#', 8), ('d#', 8), ('e*', 4), ('d', 8), ('c', 8),
+            ('b3*', 4), ('a3', 8), ('g3', 8), ('g3*', 32), ('f#3*', 32), ('g3*', 32), ('f#3*', 32), ('g3*', 32), ('f#3*', 32), ('g3*', 32), ('f#3*', 6.4), ('g3', 8), ('g3*', -2),
+        ),
+        (
+            ('g2', 8), ('f#2', 8),
+            ('e2*', 4), ('a2', 4), ('b2', 4), ('a2', 4),
+            ('g2*', 4), ('f#2', 4), ('e2', 4), ('f#2', 4),
+            ('g2*', 4), ('a2', 4), ('b2', 4), ('a2', 4),
+            ('g2*', 4), ('b2', 4), ('e2', 8), ('f#2', 8), ('g2', 8), ('f#2', 8),
+            ('e2*', 4), ('a2', 4), ('b2', 4), ('a2', 4),
+            ('g2*', 4), ('f#2', 4), ('e2', 4), ('f#2', 4),
+            ('g2*', 4), ('c3', 4), ('d3', 4), ('d3', 4),
+            ('b2*', -2),
+        )
+    )
     parent_dir =  os.getcwd()
 
-    def createDirectory (gen):
+    def createDirectory (self, gen):
         name = str(gen)
-
     # Path 
         path = os.path.join(database.parent_dir, name) 
 
@@ -29,42 +44,42 @@ class database:
 
         print("Directory '% s' created" % name) 
     
-    def createFile (path, name, data):
-        if ".wav" in name:
-            print("wav")
+    def createFile (self, path, data, name):
+        if ".wav" in path:
+            database.createWave(self,data)
+            return
         print(name, 'name')
         try:
-            os.remove(database.parent_dir+ name)
+            os.remove(database.parent_dir+ path)
         except OSError:
             print("removing old file, and creating new one")
 
-        f = open(database.parent_dir+ name, "w+")
-
+        f = open(database.parent_dir+ path, "w+")
+        
         for i in data:
-            line = ' '.join(str(x) for x in i)
+            line = ''.join(str(x) for x in i)
             f.write(line + '\n')
             
     def readFile (name):
         print(database.parent_dir +name)
         text = open(database.parent_dir +name)
-        
         file = text.readlines()
         print(file)
         text.close()
         return file
 
-    def createFile (self, song):
+    def createWave (self, song):
         for trackIndex, track in enumerate (song):
             ps.make_wav (
                 track,
-                bpm = database.m_bpm,
-                transpose = database.m_transpose,
-                pause = database.m_pause,
-                boost = database.m_boost,
-                repeat = database.m_repeat,
+                bpm = 180,
+                transpose = 1,
+                pause = 0.0,
+                boost = 1.3,
+                repeat = 1,
                 fn = self.getTrackFileName (trackIndex),
             )
-
+        
         ps.mix_files (
             *[self.getTrackFileName (trackIndex) for trackIndex in range (len (song))],
             'song.wav'
@@ -76,7 +91,6 @@ class database:
     def getTrackFileName (self, trackIndex):  
         return f'track_{str (1000 + trackIndex) [1:]}.wav'
 
-# database.createDirectory("one")
-# database.createFile("","\one\iets.wav",bach)        
-database.readFile( "\BuildingBlocks\Bach.py")
-     
+Database = database()
+Database.createDirectory("k")
+Database.createFile("\k\iets.wav",Database.bach, "bach")        
