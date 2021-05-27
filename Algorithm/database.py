c5c0af4906ed080bac64bf0e980e9339781a3d0d
Jarnoooo <43266505+Jarnoooo@users.noreply.github.com>
2021-05-27 21:12:31 +0200

    Update database.py

--- Algorithm/database.py
+++ Algorithm/database.py
@@ -2,6 +2,7 @@ import glob as gl
 import os
 import shutil
 import tomita.legacy.pysynth as ps
+from playsound import playsound
 # import tomita.legacy.pysynth as ps
 # path in methods should be treated that, folder of python file is home folder 
 
@@ -46,9 +47,27 @@ class database:
     
     def createFile (self, path, data, name):
         if ".wav" in path:
-            database.createWave(self,data)
+            for trackIndex, track in enumerate (data):
+                print(self.getTrackFileName (trackIndex))
+                ps.make_wav (
+                    track,
+                    bpm = 180,
+                    transpose = 1,
+                    pause = 0.0,
+                    boost = 1.3,
+                    repeat = 1,
+                    fn = self.getTrackFileName (trackIndex),
+                )
+            
+            ps.mix_files (
+                *[self.getTrackFileName (trackIndex) for trackIndex in range (len (data))],database.parent_dir+ path
+            )
+            
+            for fileName in gl.glob ('track_*.wav'):
+                os.remove (fileName)
             return
-        print(name, 'name')
+            
+        print(data)
         try:
             os.remove(database.parent_dir+ path)
         except OSError:
@@ -60,37 +79,21 @@ class database:
             line = ''.join(str(x) for x in i)
             f.write(line + '\n')
             
-    def readFile (name):
-        print(database.parent_dir +name)
+    def readFile (self,name):
         text = open(database.parent_dir +name)
         file = text.readlines()
         print(file)
         text.close()
         return file
 
-    def createWave (self, song):
-        for trackIndex, track in enumerate (song):
-            ps.make_wav (
-                track,
-                bpm = 180,
-                transpose = 1,
-                pause = 0.0,
-                boost = 1.3,
-                repeat = 1,
-                fn = self.getTrackFileName (trackIndex),
-            )
-        
-        ps.mix_files (
-            *[self.getTrackFileName (trackIndex) for trackIndex in range (len (song))],
-            'song.wav'
-        )
-
-        for fileName in gl.glob ('track_*.wav'):
-            os.remove (fileName)
+    def playsound (self, name):
+        print(database.parent_dir +name)
+        playsound(database.parent_dir +name)
 
     def getTrackFileName (self, trackIndex):  
         return f'track_{str (1000 + trackIndex) [1:]}.wav'
 
-Database = database()
-Database.createDirectory("k")
-Database.createFile("\k\iets.wav",Database.bach, "bach")        
+# Database = database()
+# # Database.createDirectory("k")
+# # Database.createFile("\k\iets.wav",Database.bach, "bach")        
+# Database.playsound("\k\iets.wav")
