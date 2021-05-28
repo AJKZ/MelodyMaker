b5790798dede3b3223ea6ab1697a6a075c9008be
Alan <alanjzhong@gmail.com>
2021-05-28 12:38:47 +0200

    Add note, measure, song classes

---
+++ Source/measure.py
@@ -0,0 +1,31 @@
+from note import Note
+
+class Measure:
+    def __init__(self):
+        position = 0
+        fitness_score = 0
+        notes = [Note()]*4
+
+    @property
+    def position(self):
+        return self.position
+
+    @position.setter
+    def position(self, pos):
+        self.position = pos
+    
+    @property
+    def fitness_score(self):
+        return self.fitness_score
+    
+    @fitness_score.setter
+    def fitness_score(self, score):
+        self.fitness_score = score
+
+    @property
+    def notes(self):
+        return self.notes
+
+    @notes.setter
+    def notes(self, list_of_notes):
+        self.notes = list_of_notes
---
+++ Source/song.py
@@ -0,0 +1,5 @@
+
+
+class Song:
+    def __init__(self):
+        blocks = []
